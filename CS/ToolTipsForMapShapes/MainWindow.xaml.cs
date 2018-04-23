a66e271377a10a29efddebcaddc3de952fbd6b6d
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/ToolTipsForMapShapes/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using DevExpress.Xpf.Map;
+
+namespace ToolTipsForMapShapes
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void mapControl1_MouseMove(object sender, MouseEventArgs e)
+        {
+            MapControl mapControl = sender as MapControl;
+            #region #MapControl_CalcHitInfo
+            MapShape shape = mapControl.CalcHitInfo(e.GetPosition(mapControl)).MapPath as MapShape;
+            #endregion #MapControl_CalcHitInfo
+            if (shape != null)
+            {
+                #region #MapItem_Attributes
+                string name = (string)shape.Attributes["NAME"].Value;
+                name += string.Format(": {0:n2}M", (double)shape.Attributes["POP_EST"].Value / 1000000);
+                #endregion #MapItem_Attributes
+                shapeTooltip.Show(mapControl, e.GetPosition(mapControl), name);
+            }
+            else
+            {
+                shapeTooltip.Hide();
+            }
+        }
+
+        private void mapControl1_MouseLeave(object sender, MouseEventArgs e)
+        {
+            shapeTooltip.Hide();
+        }
+    }
+}
