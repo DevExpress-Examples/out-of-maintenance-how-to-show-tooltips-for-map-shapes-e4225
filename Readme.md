e16a56bf75b46146faf6864207606e0a5780d90e
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,14 @@
+# How to show tooltips for map shapes 
+
+
+<p>This example illustrates how to display tooltips for shapes loaded from Shapefiles (<strong>Countries.dbf</strong>, <strong>Countries.shp</strong>).  </p><p>Note that a tooltip displays information (a country name, population) for each map shape from Shapefiles.</p><p></p>
+
+
+<h3>Description</h3>
+
+<p>To customize the appearance of tooltips, you can use the following properties.<br /> -&nbsp;<a href="http://help.devexpress.com/#WPF/DevExpressXpfMapVectorLayerBase_ToolTipPatterntopic">VectorLayerBase.ToolTipPattern</a>&nbsp;allows you to specify the pattern used to&nbsp;generate&nbsp;<a href="https://documentation.devexpress.com/#WPF/DevExpressXpfMapToolTipInfo_ToolTipTexttopic">ToolTipInfo.ToolTipText</a>.<u><br /> </u>-&nbsp;<a href="https://documentation.devexpress.com/#WPF/DevExpressXpfMapVectorLayerBase_ToolTipContentTemplatetopic">VectorLayerBase.ToolTipContentTemplate</a>&nbsp;allows you to specify the content template used to display tooltip data. Note that this template is bound to the&nbsp;<a href="https://documentation.devexpress.com/#WPF/clsDevExpressXpfMapToolTipInfotopic">ToolTipInfo</a>&nbsp;object.<br /> -&nbsp;<a href="https://documentation.devexpress.com/#WPF/DevExpressXpfMapMapControl_ToolTipTemplatetopic">MapControl.ToolTipTemplate</a>&nbsp;allows you to customize the&nbsp;appearance of a tooltip.</p>
+<p>Note that&nbsp;to show tooltips, the&nbsp;<a href="http://help.devexpress.com/#WPF/DevExpressXpfMapMapControl_ToolTipEnabledtopic">MapControl.ToolTipEnabled</a>&nbsp;property should be set to&nbsp;<strong>true</strong>.</p>
+
+<br/>
+
+
---
+++ VB/ShowToolTips/TypeConverters.vb
@@ -0,0 +1,68 @@
+﻿Imports DevExpress.Xpf.Map
+Imports System
+Imports System.Collections.Generic
+Imports System.ComponentModel
+Imports System.Globalization
+Imports System.Linq
+Imports System.Text
+Imports System.Windows.Data
+
+Namespace ShowToolTips
+    Friend Class MapItemPopulationAttributeToStringTypeConverter
+        Implements IValueConverter
+
+        Public Function Convert(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As CultureInfo) As Object Implements IValueConverter.Convert
+            Dim title As ShapeTitle = TryCast(value, ShapeTitle)
+            Dim item As MapItem
+            If title Is Nothing Then
+                item = TryCast(value, MapItem)
+            Else
+                item = title.MapShape
+            End If
+
+            If item Is Nothing Then
+                Return Nothing
+            End If
+
+            Dim attr = item.Attributes("POP_EST")
+            If attr Is Nothing Then
+                Return Nothing
+            End If
+
+            Return attr.Value
+        End Function
+
+        Public Function ConvertBack(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As CultureInfo) As Object Implements IValueConverter.ConvertBack
+            Throw New NotImplementedException()
+        End Function
+    End Class
+
+    Friend Class MapItemGdpAttributeToStringTypeConverter
+        Implements IValueConverter
+
+        Public Function Convert(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As CultureInfo) As Object Implements IValueConverter.Convert
+            Dim title As ShapeTitle = TryCast(value, ShapeTitle)
+            Dim item As MapItem
+            If title Is Nothing Then
+                item = TryCast(value, MapItem)
+            Else
+                item = title.MapShape
+            End If
+
+            If item Is Nothing Then
+                Return Nothing
+            End If
+
+            Dim attr = item.Attributes("GDP_MD_EST")
+            If attr Is Nothing Then
+                Return Nothing
+            End If
+
+            Return attr.Value
+        End Function
+
+        Public Function ConvertBack(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As CultureInfo) As Object Implements IValueConverter.ConvertBack
+            Throw New NotImplementedException()
+        End Function
+    End Class
+End Namespace
